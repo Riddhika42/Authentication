a58d8386a998d8857b145e7c2ed5ebe6aea1a193
Riddhika Mandal <riddhikamondal@gmail.com>
2026-03-22 23:45:57 +0530

    API(POST,GET,PUT,DELETE)

--- auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/controllers/UserController.java
+++ auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/controllers/UserController.java
@@ -15,7 +15,8 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 
 
-public class UserController {
+public class
+UserController {
 
 
     private final UserService userService;
@@ -26,6 +27,36 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<Iterable<UserDto>> getAllUsers() {
+
         return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<UserDto> getUserByEmail(@PathVariable("email") String email) {
+        return ResponseEntity.ok(userService.getUserByEmail(email));
+    }
+
+    //delete user
+    //api/v1/users/{userId}
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable("userId") String userId) {
+        userService.deleteUser(userId);
+    }
+
+    //update user
+    //api/v1/users/{userId}
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") String userId) {
+        return ResponseEntity.ok(userService.updateUser(userDto, userId));
+    }
+
+
+
+    //get user by id
+    //api/v1/users/{userId}
+   /* @PreAuthorize("hasRole('"+ AppConstants.ADMIN_ROLE +"')")*/
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable("userId") String userId) {
+        return ResponseEntity.ok(userService.getUserById(userId));
     }
 }
---
+++ auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/services/AuthService.java
@@ -0,0 +1,14 @@
+package com.substring.auth.auth_app_backend.services;
+
+
+import com.substring.auth.auth_app_backend.dtos.UserDto;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public interface AuthService {
+    UserDto registerUser(UserDto userDto);
+   // UserDto loginUser(UserDto userDto);
+}
